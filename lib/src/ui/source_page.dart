ee5db93bbfaac68bb8096dc1355255d048c2c6c1
lathifrdp <lathifrdp@gmail.com>
2022-08-14 20:25:20 +0700

    ubah stf jadi stl

--- lib/src/ui/source_page.dart
+++ lib/src/ui/source_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:liga_bola/src/helper/theme_colors.dart';
 
-class SourcePage extends StatefulWidget {
+class SourcePage extends StatelessWidget {
   const SourcePage({Key? key}) : super(key: key);
 
-  @override
-  State<SourcePage> createState() => _SourcePageState();
-}
-
-class _SourcePageState extends State<SourcePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +11,7 @@ class _SourcePageState extends State<SourcePage> {
       alignment: Alignment.center,
       padding: const EdgeInsets.all(16),
       child: Text(
-        "Aplikasi ini adalah aplikasi yang menampilkan liga dan klasemen olah raga sepak bola yang sedang berlangsung. Data ini didapatkan dari API public yang bernama Football Standings API. Berikut adalah link API tersebut. Link: https://github.com/azharimm/football-standings-api",
+        "Aplikasi ini adalah aplikasi yang menampilkan liga dan klasemen olah raga sepak bola yang sedang berlangsung. Data ini didapatkan dari API public yang bernama Football Standings API. Berikut adalah link API tersebut.\nLink: https://github.com/azharimm/football-standings-api",
         style: TextStyle(color: grayColor70),
       ),
     ));
